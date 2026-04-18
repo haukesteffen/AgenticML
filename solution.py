b4e309223440ca5d35e57782489646b888e0675d
hs <acc+github@haukesteffen.com>
2026-04-18 17:47:16 +0200

    hyperparameters: max_bin=1024 for even finer histogram splits

--- solution.py
+++ solution.py
@@ -61,7 +61,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.utils.class_weight import compute_sample_weight
 from xgboost import XGBClassifier
 
-HYPOTHESIS = "hyperparameters: max_bin=512 for finer histogram splits"
+HYPOTHESIS = "hyperparameters: max_bin=1024 for even finer histogram splits"
 
 
 def _add_features(X: pd.DataFrame) -> pd.DataFrame:
@@ -85,7 +85,7 @@ def fit_predict(
         X_train[col] = X_train[col].astype(categories)
         X_val[col] = X_val[col].astype(categories)
 
-    model = XGBClassifier(enable_categorical=True, tree_method="hist", colsample_bytree=0.8, max_bin=512)
+    model = XGBClassifier(enable_categorical=True, tree_method="hist", colsample_bytree=0.8, max_bin=1024)
     sample_weight = compute_sample_weight(class_weight="balanced", y=y_train)
     model.fit(X_train, y_train, sample_weight=sample_weight)
     return model.predict_proba(X_val)
