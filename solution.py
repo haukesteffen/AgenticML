39db2b9331b4eaaebd8363ab3251873b9dc79c57
hs <acc+github@haukesteffen.com>
2026-04-23 08:49:06 +0200

    hyperparams: tree_method=hist + n_jobs=-1 + balanced sample weights

--- solution.py
+++ solution.py
@@ -4,9 +4,10 @@ AgenticML solution module.
 
 import numpy as np
 import pandas as pd
+from sklearn.utils.class_weight import compute_sample_weight
 from xgboost import XGBClassifier
 
-HYPOTHESIS = "baseline: vanilla XGBClassifier"
+HYPOTHESIS = "hyperparams: tree_method=hist + n_jobs=-1 + balanced sample weights"
 
 
 def fit_predict(
@@ -18,6 +19,8 @@ def fit_predict(
     X_val = pd.get_dummies(X_val)
     X_val = X_val.reindex(columns=X_train.columns, fill_value=0)
 
-    model = XGBClassifier()
-    model.fit(X_train, y_train)
+    sample_weight = compute_sample_weight("balanced", y_train)
+
+    model = XGBClassifier(tree_method="hist", n_jobs=-1)
+    model.fit(X_train, y_train, sample_weight=sample_weight)
     return model.predict_proba(X_val)
