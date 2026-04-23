3f18d771e4e3e962b5105e8d8d2840d47a2f4c67
hs <acc+github@haukesteffen.com>
2026-04-23 10:45:47 +0200

    ensembling: margin-aware rare-class rescue (promote High when within 0.05 of winner)

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.utils.class_weight import compute_sample_weight
 from xgboost import XGBClassifier
 
-HYPOTHESIS = "hyperparams: balanced weights + 4.0x multiplier on rarest class (High)"
+HYPOTHESIS = "ensembling: margin-aware rare-class rescue (promote High when within 0.05 of winner)"
 
 _BASE_PARAMS = dict(tree_method="hist", n_jobs=-1, subsample=0.8, colsample_bytree=0.8, reg_lambda=2, max_bin=2048, n_estimators=150)
 
@@ -27,9 +27,23 @@ def fit_predict(
     sample_weight[y_train == rarest] *= 4.0
 
     preds = []
+    last_model = None
     for depth in [4, 6]:
         for seed in [0, 1]:
             model = XGBClassifier(**_BASE_PARAMS, max_depth=depth, random_state=seed)
             model.fit(X_train, y_train, sample_weight=sample_weight)
             preds.append(model.predict_proba(X_val))
-    return np.mean(preds, axis=0)
+            last_model = model
+
+    mean_proba = np.mean(preds, axis=0)
+
+    # Promote rarest class when within margin of current winner
+    rare_idx = np.where(last_model.classes_ == rarest)[0][0]
+    margin = 0.05
+    max_proba = mean_proba.max(axis=1)
+    rare_proba = mean_proba[:, rare_idx]
+    promote = (rare_proba >= max_proba - margin) & (rare_proba < max_proba)
+    mean_proba[promote, rare_idx] = max_proba[promote] + 1e-6
+    mean_proba /= mean_proba.sum(axis=1, keepdims=True)
+
+    return mean_proba
