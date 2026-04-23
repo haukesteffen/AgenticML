11f527170b0207d518ee96493305ff694a6d0ff4
hs <acc+github@haukesteffen.com>
2026-04-23 08:58:32 +0200

    ensembling: seed bagging over 3 XGBs (different seeds) for variance reduction

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.utils.class_weight import compute_sample_weight
 from xgboost import XGBClassifier
 
-HYPOTHESIS = "hyperparams: subsample=0.8 + colsample_bytree=0.8 for variance reduction"
+HYPOTHESIS = "ensembling: seed bagging over 3 XGBs (different seeds) for variance reduction"
 
 
 def fit_predict(
@@ -21,6 +21,9 @@ def fit_predict(
 
     sample_weight = compute_sample_weight("balanced", y_train)
 
-    model = XGBClassifier(tree_method="hist", n_jobs=-1, subsample=0.8, colsample_bytree=0.8)
-    model.fit(X_train, y_train, sample_weight=sample_weight)
-    return model.predict_proba(X_val)
+    preds = []
+    for seed in [0, 1, 2]:
+        model = XGBClassifier(tree_method="hist", n_jobs=-1, subsample=0.8, colsample_bytree=0.8, random_state=seed)
+        model.fit(X_train, y_train, sample_weight=sample_weight)
+        preds.append(model.predict_proba(X_val))
+    return np.mean(preds, axis=0)
